c0a6608027f52b90e24e8328a2cd1803887fde16
Casey Clair <caseyclair12@gmail.com>
2017-10-08 23:18:05 -0500

    added smooth scrolling for navigation

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,9 @@ $(document).ready(function(){
 		$("nav ul").toggleClass("expanded");
 	});
 });
+
+
+$(".smooth-scroll").click(function(event){
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 700);
+	});
